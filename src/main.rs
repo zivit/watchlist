332556799ee6b91dc9e0bfc02f75bad473afe719
zivit <zinc.vitaliy@gmail.com>
2024-06-06 19:29:01 +0300

    Revert "Added translation" because it doesn't work on macos
    This reverts commit b45b591e3c26a77db08ab2273df891fcfde63642.

--- src/main.rs
+++ src/main.rs
@@ -449,8 +449,6 @@ fn episode_changed(show: Show) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    slint::init_translations!(std::env::current_dir().unwrap().join("translations"));
-
     create_database()?;
     let ui = AppWindow::new()?;
     display_all_watchlist(&ui)?;
