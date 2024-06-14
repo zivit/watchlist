85cfd9b1912d0da49280bf536b6df5ea8ad02040
zivit <zinc.vitaliy@gmail.com>
2024-06-14 12:07:09 +0300

    Changed "check new episodes" function

--- src/datetime.rs
+++ src/datetime.rs
@@ -1,31 +1,23 @@
-use anyhow::{bail, Context, Ok, Result};
+use anyhow::{bail, Ok, Result};
 use chrono::prelude::*;
 
 pub fn check_new_episodes_available(time: &str, current_episode: u32, schedule: [u32; 7]) -> Result<bool> {
-    let parsed_date = NaiveDateTime::parse_from_str(time, "%Y-%m-%d %H:%M")
-        .with_context(|| format!("Failed to parse release time: {}", time))?;
-    let release_time = match Local.from_local_datetime(&parsed_date) {
-        chrono::offset::LocalResult::Single(t) => t,
-        chrono::offset::LocalResult::Ambiguous(_, _) => {
-            bail!("Failed to convert naive time to local time")
-        }
-        chrono::offset::LocalResult::None => bail!("Failed to convert naive time to local time"),
-    };
-    let time_now = Local::now();
-    let time_elapsed = time_now.signed_duration_since(release_time);
-    if time_elapsed.num_weeks() < 0 {
+    let start_time = NaiveDateTime::parse_from_str(time, "%Y-%m-%d %H:%M")?;
+    let now = Local::now().naive_local();
+
+    let duration_since_start = now.signed_duration_since(start_time);
+    if duration_since_start.num_days() < 0 {
         bail!("Release time is in the future");
     }
 
-    let weeks_count = time_elapsed.num_weeks() + 1;
-    let episodes_per_week = schedule.iter().sum::<u32>();
-    let episodes_elapsed = episodes_per_week * weeks_count as u32;
-    let episodes_elapsed_this_week: u32 = schedule
-        .iter()
-        .enumerate()
-        .filter(|(i, d)| **d > 0 && time_now.weekday() as usize > *i)
-        .map(|(_, d)| *d)
-        .sum();
+    let days_since_start = duration_since_start.num_days() as usize;
+    let start_day_of_week = start_time.weekday().num_days_from_monday() as usize;
 
-    Ok(episodes_elapsed + episodes_elapsed_this_week > current_episode)
+    let mut total_episodes = 0;
+    for i in 0..=days_since_start {
+        let day_of_week = (start_day_of_week + i) % 7;
+        total_episodes += schedule[day_of_week];
+    }
+
+    Ok(total_episodes > current_episode)
 }
